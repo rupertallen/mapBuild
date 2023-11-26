302296db2d869697dda00b3007d0b2e3d0d16e95
Rupert Allen <rupert.allen@gmail.com>
2023-11-26 13:11:22 -0500

    building islands

--- mapBuild/Form1.cs
+++ mapBuild/Form1.cs
@@ -16,13 +16,14 @@ namespace pdfScratcher
         const string PDFLocation = "C:\\Users\\ruper\\OneDrive\\Desktop\\Output.pdf";
 
         enum MapSize
-        {
+        {           
             Tiny = 11,
             Small = 14,
             Normal = 16,
             Large = 18,
             Huge = 20,
-            Massive = 30
+            Massive = 30,
+            SuperSized = 50
         }
 
         enum MapType
@@ -31,7 +32,8 @@ namespace pdfScratcher
             Lakes = 2,
             Continents = 3,
             Archipelago = 4,
-            WaterWorlds = 5
+            WaterWorlds = 5,
+            Mountains = 6
         }
 
         enum MapSquareType
@@ -42,14 +44,22 @@ namespace pdfScratcher
             Mountain = 4
         }
 
+        enum Direction
+        {
+            North = 1,
+            South = 2,
+            East = 3,
+            West = 4
+        }
+
         private PdfBrush MapSquareColor(MapSquareType squareType)
         {
             switch (squareType)
             {
                 case MapSquareType.Ocean:
-                    return PdfBrushes.SlateBlue;
+                    return PdfBrushes.CornflowerBlue;
                 case MapSquareType.Sea:
-                    return PdfBrushes.LightBlue;
+                    return PdfBrushes.SkyBlue;
                 case MapSquareType.Land:
                     return PdfBrushes.Green;
                 case MapSquareType.Mountain:
@@ -64,12 +74,13 @@ namespace pdfScratcher
             Default = 500
         }
 
-        private PdfDocument CreateMap(MapSize mapsize, CanvasSize canvassize)
+        private PdfDocument CreateMap(MapSize mapsize, CanvasSize canvassize, MapType maptype)
         {
             PdfDocument doc = new PdfDocument();
             PdfPageBase page = doc.Pages.Add();
             PdfGrid grid = new PdfGrid();
             grid.Style.CellPadding = new PdfPaddings(1, 1, 1, 1);
+            
             grid.Columns.Add((int)mapsize);
             for (int i = 0; i < (int)mapsize; i++)
             {
@@ -83,40 +94,82 @@ namespace pdfScratcher
             {
                 row.Height = (int)canvassize / (int)mapsize;
             }
-            grid = CreateGeography(grid, mapsize);
+            grid = CreateGeography(grid, mapsize, maptype);
             grid.Draw(page, new PointF(10, 10));
             return doc;
         }
 
-        private PdfGrid CreateGeography(PdfGrid grid, MapSize mapsize)
+        private PdfGrid CreateGeography(PdfGrid grid, MapSize mapsize, MapType maptype)
         {
-            foreach (PdfGridRow row in grid.Rows)
+            switch (maptype)
             {
-                row.Style.BackgroundBrush = MapSquareColor(MapSquareType.Ocean);
+                case MapType.Drylands:
+                    foreach (PdfGridRow row in grid.Rows)
+                    {
+                        row.Style.BackgroundBrush = MapSquareColor(MapSquareType.Land);
+                    }
+                    return grid;
+                case MapType.WaterWorlds:
+                    foreach (PdfGridRow row in grid.Rows)
+                    {
+                        row.Style.BackgroundBrush = MapSquareColor(MapSquareType.Ocean);
+                    }
+                    int islandCount = (int)mapsize / 4;
+                    Debug.WriteLine("islandCount = " + islandCount);
+                    for (int i = 0;i < islandCount;i++)
+                    {
+                        int startX = new Random().Next((int)mapsize - 1);
+                        int startY = new Random().Next((int)mapsize - 1);
+                        grid.Rows[startX].Cells[startY].Style.BackgroundBrush = MapSquareColor(MapSquareType.Land);
+                        int islandSize = new Random().Next(1, (int)mapsize * 2);
+                        int x = startX;
+                        int y = startY;
+                        for (int s = 0;s < islandSize;s++)
+                        {
+                            int chance = new Random().Next(1, 100);
+                            if (chance > 50) { x = startX; y = startY; }
+                            int d = new Random().Next(1, 4);
+                            Direction direction = (Direction)d;
+                            switch (direction)
+                            {
+                                case Direction.North:
+                                    if (y != 0) y--;
+                                    break;
+                                case Direction.South:
+                                    if (y != (int)mapsize - 1) y++;
+                                    break;
+                                case Direction.East:
+                                    if (x != (int)mapsize - 1) x++;
+                                    break;
+                                case Direction.West:
+                                    if (x != 0) x--;
+                                    break;
+                                default:
+                                    throw new NotImplementedException();
+                            }
+                            int typeChance = new Random().Next(1, 100);
+                            if (typeChance > 10)
+                            {
+                                grid.Rows[x].Cells[y].Style.BackgroundBrush = MapSquareColor(MapSquareType.Land);
+                            }
+                            else
+                            {
+                                grid.Rows[x].Cells[y].Style.BackgroundBrush = MapSquareColor(MapSquareType.Mountain);
+                            }
+                        }
+                    }
+                    return grid;
+                default:
+                    throw new NotImplementedException();
             }
-
-            int islandCount = (int)mapsize / 4;
-            Debug.WriteLine("islandCount = " +  islandCount);
-
-            int startX = new Random().Next((int)mapsize - 1);
-            int startY = new Random().Next((int)mapsize - 1);
-
-            grid.Rows[startX].Cells[startY].Style.BackgroundBrush = MapSquareColor(MapSquareType.Land);
-
-
-            //grid.Rows[4].Cells[4].Style.BackgroundBrush = MapSquareColor(MapSquareType.Land);
-            //grid.Rows[4].Cells[5].Style.BackgroundBrush = MapSquareColor(MapSquareType.Mountain);
-            //grid.Rows[5].Cells[4].Style.BackgroundBrush = MapSquareColor(MapSquareType.Sea);
-            //grid.Rows[5].Cells[5].Style.BackgroundBrush = MapSquareColor(MapSquareType.Sea);
-
-            return grid;
         }
 
         private void btnGeneratePDF_Click(object sender, EventArgs e)
         {
             //MapSize size;
             //Enum.TryParse<MapSize>(comboMapSize.SelectedValue.ToString(), out size);
-            CreateMap(MapSize.Tiny, CanvasSize.Default).SaveToFile(PDFLocation);
+            CreateMap(MapSize.Normal, CanvasSize.Default, MapType.WaterWorlds).SaveToFile(PDFLocation);
+            Process.Start("cmd", "/c start msedge " + PDFLocation);
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -126,7 +179,7 @@ namespace pdfScratcher
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            btnGeneratePDF_Click((object)this, EventArgs.Empty);
+            CreateMap(MapSize.Normal, CanvasSize.Default, MapType.WaterWorlds).SaveToFile(PDFLocation);
             Process.Start("cmd", "/c start msedge " + PDFLocation);
             foreach (var item in Enum.GetValues(typeof(MapSize)))
             {
